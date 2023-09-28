b8bf45a69a0c9f0c16a674ae907b15798285876e
sivan <642895350@qq.com>
2023-09-28 09:17:07 +0800

    增加触摸面板功能

---
+++ USER/touch_panel.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: xw.qu
+ * @Date: 2023-09-28 09:14:31
+ * @LastEditors: xw.qu
+ * @LastEditTime: 2023-09-28 09:15:45
+ * @FilePath: \USER\touch_panel.c
+ * @Description: ´¥ÃþÃæ°å
+ * 
+ * Copyright (c) 2023 by xw.qu, All Rights Reserved. 
+ */
+
+#include "touch_panel.h"
+#include "sys.h"
+#include "T5LLIB.h"
+
---
+++ USER/touch_panel.h
@@ -0,0 +1,17 @@
+/*
+ * @Author: xw.qu
+ * @Date: 2023-09-28 09:14:31
+ * @LastEditors: xw.qu
+ * @LastEditTime: 2023-09-28 09:16:23
+ * @FilePath: \USER\touch_panel.h
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by xw.qu, All Rights Reserved. 
+ */
+
+
+#ifndef __TOUCH_PANEL_H__
+#define __TOUCH_PANEL_H__
+
+
+#endif
